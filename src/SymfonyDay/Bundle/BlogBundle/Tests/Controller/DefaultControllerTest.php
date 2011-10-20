c388ea60044a54d594481535c038fbabd0d11159
Per Sandström <per@helloper.com>
2011-10-20 15:37:50 +0200

    Test for SQL query count

--- src/SymfonyDay/Bundle/BlogBundle/Tests/Controller/DefaultControllerTest.php
+++ src/SymfonyDay/Bundle/BlogBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,12 +10,19 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
+        // Set a crawler to make a fake request
         $crawler = $client->request('GET', '/blog');
 
+        // Count instances with CSS3 syntax
         $this->assertEquals(2, $crawler->filter('.post')->count());
         $this->assertEquals(1, $crawler->filter('html:contains("Symfony Day")')->count());
 
+        // Use a regexp to look for a certain word
         $response = $client->getResponse();
         $this->assertRegExp('/Second/', $response->getContent());
+
+        // Check if there are more than five SQL queries
+        $profile = $client->getProfile();
+        $this->assertLessThanOrEqual(5, $profile->getCollector('db')->getQueryCount());
     }
 }
